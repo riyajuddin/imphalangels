3b224650813c8a8ae80fc233021cb6c5d4c77e14
Md Riyajuddin <mdriya93@gmail.com>
2019-01-08 10:55:51 +0530

    database

--- imphal_angels (2).sql
+++ imphal_angels (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 07, 2019 at 07:24 AM
+-- Generation Time: Jan 08, 2019 at 05:25 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `company_details` (
   `user_amount` varchar(20) NOT NULL,
   `pitching` int(2) NOT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `company_details`
@@ -56,7 +56,36 @@ INSERT INTO `company_details` (`company_id`, `company_name`, `co_founder_no`, `c
 (4, 'sdsfs', 0, 'xfgfsgsfg@sfdsdf.sdf', '43534543', 1, '1546844365Tulips.jpg', '.jpg', '606.34', 1, '4300', 1),
 (5, 'sdsfs', 0, 'xfgfsgsfg@sfdsdf.sdf', '43534543', 0, '1546844378Tulips.jpg', '.jpg', '606.34', 2, '2100', 1),
 (6, 'sdsfs', 0, 'xfgfsgsfg@sfdsdf.sdf', '43534543', 0, '', '', '', 0, '500', 0),
-(7, 'sffsfd', 0, 'sdfsdfsdfsdfsdfsdfds', '23423443242342342342', 0, '1546844661INVOICE_xlsx.pdf', '.pdf', '360.04', 0, '500', 1);
+(7, 'sffsfd', 0, 'sdfsdfsdfsdfsdfsdfds', '23423443242342342342', 0, '1546844661INVOICE_xlsx.pdf', '.pdf', '360.04', 0, '500', 1),
+(8, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(9, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(10, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(11, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(12, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(13, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(14, 'Mobimp', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(15, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(16, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(17, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(18, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(19, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(20, 'dfgfgd', 0, 'sdfsdfsd@sdf.sdf', '2132133121', 0, '', '', '', 0, '500', 0),
+(21, 'IMPHAL', 0, 'mdriya93@gmail.com', '1234567890', 1, '', '', '', 1, '4300', 0),
+(22, 'teste', 0, 'sdflksjfsd@dxfdfsd.s', '23423423', 0, '', '', '', 0, '500', 0),
+(23, 'sfsdfsd', 0, 'sdfsdfsdf@dfgdfg.sdf', '234342', 0, '', '', '', 0, '500', 0),
+(24, 'fdgfdgd', 0, 'hghdf@jfhg.hdfd', '43143143134', 0, '', '', '', 0, '500', 0),
+(25, 'dfsdfsd', 0, 'sdfsdfds@sdfsdfs.sdf', '234324', 0, '', '', '', 0, '500', 0),
+(26, 'xfgfgfd', 0, 'sfsdfsdf@sdfsdf.sdf', '3453453', 0, '', '', '', 0, '500', 0),
+(27, 'hgfhgfgh', 0, 'gfgdf@hfhgfh.hgf', '4324324', 0, '', '', '', 0, '500', 0),
+(28, 'hghghd', 0, 'gfdfgdfd2@gfdgfdfgd.', '4343232', 0, '', '', '', 0, '500', 0),
+(29, 'slkjsdflkj', 0, 'dsflkjsd@sdaf.sdf', '23342', 0, '', '', '', 0, '500', 0),
+(30, 'slkjsdflkj', 0, 'dsflkjsd@sdaf.sdf', '23342', 0, '', '', '', 0, '500', 0),
+(31, 'xffgd', 0, 'sfsdfsdf@sdfsdf.sdf', '2342342', 0, '', '', '', 0, '500', 0),
+(32, 'dfglkjfdlkj', 0, 'lkjsdflkjfs@flskdfj.', '23424', 0, '', '', '', 0, '500', 0),
+(33, 'mobimp', 0, 'test@gmail.com', '12324324234', 0, '', '', '', 0, '500', 0),
+(34, 'test company', 0, 'sdflksjfsd@dxfdfsd.s', '8765656567', 0, '', '', '', 0, '500', 0),
+(35, 'test', 0, 'sdflksjfsd@dxfdfsd.s', '2121212121', 0, '', '', '', 0, '500', 0),
+(36, 'test', 0, 'sdflksjfsd@dxfdfsd.s', '2121212121', 0, '', '', '', 0, '500', 0);
 
 -- --------------------------------------------------------
 
@@ -70,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `company_user_details` (
   `company_id` varchar(100) NOT NULL,
   `user_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `company_user_details`
@@ -96,7 +125,45 @@ INSERT INTO `company_user_details` (`user_id`, `company_id`, `user_name`) VALUES
 (17, '6', '2nd name'),
 (18, '6', '3rd name'),
 (19, '7', 'sdfsdfs'),
-(20, '8', 'sdfsdfs');
+(20, '8', 'sdfsdfs'),
+(21, '8', 'Tomba 1'),
+(22, '8', 'Tomba 2'),
+(23, '9', 'Tomba 1'),
+(24, '9', 'Tomba 2'),
+(25, '10', 'Tomba 1'),
+(26, '10', 'Tomba 2'),
+(27, '11', 'Tomba 1'),
+(28, '11', 'Tomba 2'),
+(29, '12', 'Tomba 1'),
+(30, '12', 'Tomba 2'),
+(31, '13', 'Tomba 1'),
+(32, '13', 'Tomba 2'),
+(33, '14', 'Tomba 1'),
+(34, '14', 'Tomba 2'),
+(35, '15', 'sdfsdf'),
+(36, '16', 'sdfsdf'),
+(37, '17', 'sdfsdf'),
+(38, '18', 'sdfsdf'),
+(39, '19', 'sdfsdf'),
+(40, '20', 'sdfsdf'),
+(41, '21', 'test name 1'),
+(42, '21', 'test name 2'),
+(43, '22', 'sdsfd'),
+(44, '22', 'sfddfsd'),
+(45, '23', 'sdfsfd'),
+(46, '24', 'fdsfds'),
+(47, '25', '1fgdf'),
+(48, '26', 'dsfsfd'),
+(49, '27', 'gfd'),
+(50, '28', '1fgdf'),
+(51, '29', 'dsfsfd'),
+(52, '30', 'dsfsfd'),
+(53, '31', '234ds'),
+(54, '32', '1'),
+(55, '33', 'test'),
+(56, '34', 'ererere'),
+(57, '35', 'test name 1'),
+(58, '36', 'test name 1');
 
 -- --------------------------------------------------------
 
